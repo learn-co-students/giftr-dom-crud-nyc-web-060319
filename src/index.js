ca7b6c8b12d17ea475b4c26b14ab0749dc5d31a9
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-07-18 13:08:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM has been fully loaded');
   console.table(gifts);
-
+  let giftsCopy = gifts.slice();
   let giftId = 5;
-  
-  const giftsList = document.querySelector(".gift-list");
-  giftsList.style.textAlign = "center";
-
+  displayGifts(giftsCopy);
+  let giftsList = document.querySelector(".gift-list");
+  console.log(giftsList)
   document.querySelector('#gift-form').addEventListener('submit', createGift)
+  v1 = document.querySelector('#filter-input')
+  v1.addEventListener('change', function(e){
+    e.preventDefault();	
+    console.log(v1.value);	
+  })
 
-  function displayGifts(){
-      for(let i = 0 ; i < gifts.length ; i++){
-        let gift = document.createElement('li');
-        gift.innerText = gifts[i].name;
+  function displayGifts(giftsCopy){
+      for(let i = 0 ; i < giftsCopy.length ; i++){
+        const gift = document.createElement('li');
+        gift.innerText = giftsCopy[i].name;
         gift.style.textDecoration ="none";
         gift.style.textTransform = "capitalize";
         let giftImage = document.createElement('img');
-        giftImage.src = gifts[i].image;
+        giftImage.src = giftsCopy[i].image;
         giftImage.style.width = '10em';
         giftImage.style.display = 'block';
         gift.appendChild(giftImage);
+        let giftsList = document.querySelector(".gift-list");
         giftsList.appendChild(gift);
+        deleteGift(gift);
       }
     }
 
+  function deleteGift(gift){
+    const deleteBtn = document.createElement("button");
+    deleteBtn. innerHTML = "Do Something";
+    gift.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', function(){
+      gift.remove();
+    })
+  }
 
   function createGift(e){
     e.preventDefault();
     let nameInput = document.getElementById('gift-name-input');
     let imageInput = document.getElementById('gift-image-input');
-    gifts.push({id: giftId++, name: nameInput.value, image: imageInput.value})
+    giftsCopy.unshift({id: giftId++, name: nameInput.value, image: imageInput.value})
     nameInput.innerText = "";
     imageInput.innerText = "";
-    displayGifts();
+    displayGifts(giftsCopy);
   }
-  displayGifts();
+  
 })
